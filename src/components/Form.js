77247f6a53b24ecd564b3520cab070c3301f9250
Ricardo Diaz <Ricardo.Diaz@endava.com>
2020-01-20 10:47:01 -0500

    passing optiones to state of cryptocurrency

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import useMoney from '../hooks/useMoney';
-import useCryproCurrency from '../hooks/useCryptoCurrency';
 import useCryptoCurrency from '../hooks/useCryptoCurrency';
+import axios from 'axios';
+
 const Button = styled.input`
     margin-top: 20px;
     font-weight: bold;
@@ -20,19 +21,30 @@ const Button = styled.input`
     }
 `
 const Form = () => {
+    const [listCrypto, setListCrypto] = useState([]);
+
     const CURRENCY = [
-        {id : 'COL' , name : 'Peso Colombiano'},
-        {id : 'USD' , name : 'Dolar Estados Unidos'},
-        {id : 'MXN' , name : 'Peso Mexicano'},
-        {id : 'EUR' , name : 'Euro'},
-        {id : 'GBP' , name : 'Libra Esterlina'}
+        { id: 'COL', name: 'Peso Colombiano' },
+        { id: 'USD', name: 'Dolar Estados Unidos' },
+        { id: 'MXN', name: 'Peso Mexicano' },
+        { id: 'EUR', name: 'Euro' },
+        { id: 'GBP', name: 'Libra Esterlina' }
     ]
-    const [money , SelectMoney ] = useMoney('Elige la moneda' ,'',CURRENCY);
-    const [crypto, SelectCrypto] = useCryptoCurrency('Elige Criptomoneda' ,'')
+    const [money, SelectMoney] = useMoney('Elige la moneda', '', CURRENCY);
+    const [crypto, SelectCrypto] = useCryptoCurrency('Elige Criptomoneda', '', listCrypto);
+
+    useEffect(() => {
+        const queryAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            const { data } = await axios.get(url);
+            setListCrypto(data.Data);
+        }
+        queryAPI();
+    }, []);
     return (
         <form>
-            <SelectMoney/>
-            <SelectCrypto/>
+            <SelectMoney />
+            <SelectCrypto />
             <Button
                 type="submit"
                 value="calcular"
